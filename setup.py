e58718ccd891521b553ee12bf25c6060d815b4bb
Divya Goteti <digoteti@in.ibm.com>
2019-10-16 20:19:10 +0530

    intial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # Here is the module name.
+    name="dbsplitsql",
+
+    # version of the module
+    version="0.0.1",
+
+    # Name of Author
+    author="Divya Goteti",
+
+    # your Email address
+    author_email="balagdivya@gmail.com",
+
+    # Small Description about module
+    description="It will split all the table and views of a schema in a file, into its respective files",
+
+    long_description=long_description,
+
+    # Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+
+    # Any link to reach this module, if you have any webpage or github profile
+    url="https://github.com/balagdivya/dbsplitsql",
+    packages=setuptools.find_packages(),
+
+    # classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
